9aecb777e296fccb49fe158bc9e2240bd82e6796
Johann PC <jteske0104@gmail.com>
2024-07-05 16:07:41 +0200

    chore: update server start message

--- server/server.js
+++ server/server.js
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV !== 'production') {
 const logger = require('./logger');
 const express = require('express');
 const app = express();
-const port = process.env.BACKEND_PORT || 5000;
 const bodyParser = require('body-parser');
+
+const port = process.env.BACKEND_PORT || 5000;
 
 // body parsers
 // create application/json parser
@@ -23,20 +24,81 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 //add parsers to express
 app.use(jsonParser, textParser, urlencodedParser);
 
+// Route Specific Middleware Type 1 (add to route as middleware) - called second
 const middleware1 = (req, res, next) => {
-  logger.debug('Call Middleware 1');
+  logger.trace('Call Middleware 1');
   // Tasks
   next();
 };
 
-const middleware2 = (req, res, next) => {
-  logger.debug('Call Middleware 2');
+// Route Specific Middleware Type 2 - called first
+app.all('/api', (req, res, next) => {
+  logger.trace('Call Middleware 2');
   // Tasks
+  next();
+});
+
+const logFullURLMiddleware = function (req, res, next) {
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const url = req.originalUrl;
+
+  const fullUrl = `${protocol}://${host}:${port}${url}`;
+
+  const responseString = `Full URL is: ${fullUrl}`;
+  logger.trace(responseString);
   next();
 };
 
-app.use('/api', middleware1, middleware2, require('./routes/api/index'));
+// Mount routers
+app.use(
+  '/api',
+  logFullURLMiddleware,
+  middleware1,
+  require('./routes/api/index')
+);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Handle custom direct access tries which are not going to /api
+//app.get('*', function (req, res) {
+//  res.status(404).send('Access denied!');
+//});
+
+const baseURL = (req, res, next) => {
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const url = req.originalUrl;
+  console.log(protocol);
+
+  const fullUrl = `${protocol}://${host}:${port}${url}`;
+
+  const responseString = `Full URL is: ${fullUrl}`;
+  console.log(responseString);
+  next();
+};
+
+// Function to get the base URL
+function getBaseUrl(server) {
+  const address = server.address();
+  const host = address.address === '::' ? 'localhost' : address.address;
+  const port = address.port;
+  return `http://${host}:${port}`;
+}
+
+// Start Server and listen on Port
+const server = app.listen(port, (error) => {
+  const baseUrl = getBaseUrl(server);
+
+  if (!error) {
+    console.log();
+    console.log('##################');
+    console.log();
+    console.log(
+      `Server is Successfully Running, and listening on port ${port}`
+    );
+    console.log();
+    console.log(`Visit the Server on the following Link: ${baseUrl}`);
+    console.log();
+    console.log('##################');
+    console.log();
+  } else console.log("Error occurred, server can't start", error);
 });
