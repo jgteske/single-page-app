923ca15224176818fb780d44f25be9b7cea3f618
Johann PC <jteske0104@gmail.com>
2024-07-14 16:16:52 +0200

    chore: add example how to use i18n translation for about page

--- frontend/src/routes/about.lazy.tsx
+++ frontend/src/routes/about.lazy.tsx
@@ -1,9 +1,16 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createLazyFileRoute('/about')({
   component: About,
 });
 
 function About() {
-  return <div className='p-2'>Hello from About!</div>;
+  const { t } = useTranslation();
+  return (
+    <div className='p-2'>
+      <h3>{t('main:page.about.title')}</h3>
+      <p>{t('main:page.about.description')}</p>
+    </div>
+  );
 }
